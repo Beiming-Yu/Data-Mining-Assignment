7f1c929a219fed61623e248cd26d5c99b5f62a08
-Beiming- <57883784+Beiming-Yu@users.noreply.github.com>
2022-05-12 18:58:55 +0800

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,15 +1,34 @@
 import os
 import numpy as np
+import scipy.io as scio
 
-def load(name):
+
+def load_GeoLife(name):
     with open(name, "r") as file:
         data = file.read().split('\n')[6:-1]
         mode = name.split("_")[1][:-4]
         latitude = np.array([item.split(',')[0] for item in data])
         longitude = np.array([item.split(',')[1] for item in data])
-        return latitude, longitude, mode
+        location = np.concatenate((latitude, longitude))
+        return location, mode
 
 
-filenames = os.listdir("H:/下载/Geolife Trajectories 1.3/result/")
-for filename in filenames:
-    load("H:/下载/Geolife Trajectories 1.3/result/" + filename)
+def load_TRAFFIC(data_path):
+    data = scio.loadmat(data_path)
+    labels = data['truth']
+    data = data['tracks_traffic']
+    mode = []
+    location = []
+    for i in range(len(labels)):
+        if labels[i][1] == 1:
+            mode.append(labels[i][0])
+            tem = data[i][0].T
+            location.append(tem)
+    return np.array(location), np.array(mode)
+
+# filenames = os.listdir("H:/下载/Geolife Trajectories 1.3/result/")
+# for filename in filenames:
+#     data, mode = load_GeoLife("H:/下载/Geolife Trajectories 1.3/result/" + filename)
+
+# filename = "H:/下载/TRAFFIC/TRAFFIC.mat"
+# data, mode = load_TRAFFIC(filename)
